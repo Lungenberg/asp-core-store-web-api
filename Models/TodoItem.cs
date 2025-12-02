936147290b5f31e9353a6e83263c0fb405d25edc
NichitaNegru <113926694+Lungenberg@users.noreply.github.com>
2025-12-02 19:16:08 +0200

    Revert "added simple exception handler (global and for http request)"

---
+++ Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPCoreWebApplication.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = "";
+
+        public bool isDone { get; set; }
+    }
+}
